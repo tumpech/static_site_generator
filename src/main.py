6b5af5f116c496151ad7342fc5c59dd38f97ebef
tumpech <laszlo.tumpech@proton.me>
2025-01-16 16:49:18 +0100

    CH2-L1

---
+++ src/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from textnode import TextNode, TextType
+
+def main():
+    print(TextNode("This is a text node",TextType.BOLD,"https://www.boot.dev"))
+
+if __name__ == "__main__":
+    main()
