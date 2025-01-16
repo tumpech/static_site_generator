9b49fc46651478850ef1fbd1b0323dd17492bbb1
tumpech <laszlo.tumpech@proton.me>
2025-01-16 19:06:31 +0100

    CH2-L6

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -17,11 +17,11 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is an image", TextType.IMAGE, "public/image.png")
         self.assertEqual(node1, node2)
     def test_text_neq(self):
-        node1 = TextNode("This is a text node", TextType.NORMAL)
-        node2 = TextNode("This is another text node", TextType.NORMAL)
+        node1 = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is another text node", TextType.TEXT)
         self.assertNotEqual(node1, node2)
     def test_mode_neq(self):
-        node1 = TextNode("This is a text node", TextType.NORMAL)
+        node1 = TextNode("This is a text node", TextType.TEXT)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertNotEqual(node1, node2)
     def test_text_mode_neq(self):
@@ -44,7 +44,19 @@ class TestTextNode(unittest.TestCase):
         node1 = TextNode("This is a link to boot.dev", TextType.LINK, "https://www.boot.dev/")
         node2 = TextNode("This is an image", TextType.IMAGE, "public/image.jpg")
         self.assertNotEqual(node1, node2)
-    
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text")
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "public/image.jpg")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props,{"src": "public/image.jpg", "alt": "This is an image"})
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,8 @@
+from htmlnode import LeafNode
 from enum import Enum
 
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -18,3 +19,20 @@ class TextNode():
         return self.text == other.text and self.text_type == other.text_type and self.url == other.url
     def __repr__(self):
         return f"TextNode({self.text},{self.text_type.value},{self.url})"
+
+def text_node_to_html_node(text_node):
+    match(text_node.text_type):
+        case(TextType.TEXT):
+            return LeafNode(None,text_node.text)
+        case(TextType.BOLD):
+            return LeafNode("b",text_node.text)
+        case(TextType.ITALIC):
+            return LeafNode("i",text_node.text)
+        case(TextType.CODE):
+            return LeafNode("code",text_node.text)
+        case(TextType.ITALIC):
+            return LeafNode("a",text_node.text,{"href": text_node.url})
+        case(TextType.IMAGE):
+            return LeafNode("i","",{"src": text_node.url, "alt": text_node.text})
+        case _:
+            ValueError(f"Invalid text type: {text_node.text_type}")
