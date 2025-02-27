2ae719eda7c299ddc0cb37af041765e9ee4b79f5
tumpech <laszlo.tumpech@proton.me>
2025-02-27 10:17:33 +0100

    Change italic to "_" from "*"

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -66,6 +66,7 @@ def text_to_textnodes(text):
     initial_node = TextNode(text, TextType.TEXT)
     nodes = split_nodes_delimiter([initial_node], "**", TextType.BOLD)
     nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
     nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
     nodes = split_nodes_image(nodes)
     nodes = split_nodes_link(nodes)
